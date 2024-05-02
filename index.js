85573e0e5fd7f9d147f5058d94bd23d953b034e6
ilona <gogiashvili.ilona@gmail.com>
2024-05-02 13:10:37 -0400

    commit tree 0f3298725289b9013ef64b895713f2f17b546aaa

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+function findMatching(names, str) {
+  let result = names.filter((name) => name === str);
+  //   for (let i = 0; i < result.length; i++) {
+  //     return result[i] === str;
+  //   }
+
+  return result;
+}
+function fuzzyMatch(names, str) {
+  let result = names.filter((name) => name[0] === str[0]);
+  return result;
+}
+function matchName(driver, str) {
+  let result = driver.filter((driver) => driver.name === str);
+  return result;
+}
